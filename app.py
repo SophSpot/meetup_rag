ef1324470a3a4d4307ec74aaa4a5d070231008ec
Sophia Loris <sophia@lor.is>
2023-11-29 20:45:28 -0600

    Formatting

--- app.py
+++ app.py
@@ -30,17 +30,20 @@ prompt = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
 
+
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
+
 def ask_question_with_context(question):
     rag_chain = (
-        {"context": retriever | format_docs , "question": RunnablePassthrough()}
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
         | prompt
         | ChatOpenAI(model_name="gpt-3.5-turbo")
         | StrOutputParser()
     )
     return rag_chain.invoke(question)
+
 
 with st.sidebar:
     st.header("Ask Me About FC Cincinnati!")
@@ -48,14 +51,19 @@ with st.sidebar:
 st.title("Ask Me About FC Cincinnati!")
 
 if "messages" not in st.session_state:
-    st.session_state['messages'] = [{'role': 'assistant', 'content': 'Ask me questions related to Ask Me About FC Cincinnatis 2023 Season!'}]
+    st.session_state["messages"] = [
+        {
+            "role": "assistant",
+            "content": "Ask me questions related to Ask Me About FC Cincinnatis 2023 Season!",
+        }
+    ]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if user_question := st.chat_input():
-    st.session_state.messages.append({'role': 'user', 'content': user_question})
+    st.session_state.messages.append({"role": "user", "content": user_question})
     st.chat_message("user").write(user_question)
     answer = ask_question_with_context(user_question)
     st.chat_message("assistant").write(answer)
-    st.session_state.messages.append({'role': 'assistant', 'content': answer})
+    st.session_state.messages.append({"role": "assistant", "content": answer})
